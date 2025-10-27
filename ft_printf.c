f2a1678bc8972932e9ac706da00ab36970c62a97
Asmae Mouncif <asmounci@c3r2p2.1337.ma>
2025-10-27 19:46:36 +0100

    final version

--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,35 @@
 /*   By: asmounci <asmounci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 09:01:16 by asmounci          #+#    #+#             */
-/*   Updated: 2025/10/27 18:38:56 by asmounci         ###   ########.fr       */
+/*   Updated: 2025/10/27 19:44:45 by asmounci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+int	ft_format(const char *format, int i, va_list args)
+{
+	int	count;
+
+	count = 0;
+	if (format[i] == 'c')
+		count = count + ft_putchar(va_arg(args, int));
+	else if (format[i] == 's')
+		count = count + ft_putstr(va_arg(args, char *));
+	else if (format[i] == 'd' || format[i] == 'i')
+		count = count + ft_putnbr(va_arg(args, int));
+	else if (format[i] == 'u')
+		count = count + ft_putnbr_unsigned(va_arg(args, unsigned int));
+	else if (format[i] == 'p')
+		count = count + ft_putptr(va_arg(args, void *));
+	else if (format[i] == 'x')
+		count = count + ft_puthex_lower(va_arg(args, unsigned int));
+	else if (format[i] == 'X')
+		count = count + ft_puthex_upper(va_arg(args, unsigned int));
+	else if (format[i] == '%')
+		count = count + ft_putchar('%');
+	return (count);
+}
 
 int	ft_printf(const char *format, ...)
 {
@@ -26,20 +50,7 @@ int	ft_printf(const char *format, ...)
 		if (format[i] == '%' && format[i + 1] != '\0')
 		{
 			i++;
-			if (format[i] == 'c')
-				count = count + ft_putchar(va_arg(args, int));
-			else if (format[i] == 's')
-				count = count + ft_putstr(va_arg(args, char *));
-            else if (format[i] == 'd' || format[i] == 'i')
-                count = count + ft_putnbr(va_arg(args, int));
-            else if (format[i] == 'u')
-                count = count + ft_putnbr_unsigned(va_arg(args, int));
-            else if	(format[i] == 'p')
-				count = count + ft_putptr(va_arg(args, void *));
-			else if(format[i] == 'x')
-				count = count + ft_puthex_lower(va_arg(args, int));
-			else if(format[i] == 'X')
-				count = count + ft_puthex_upper(va_arg(args, int));
+			count = count + ft_format(format, i, args);
 		}
 		else
 		{
