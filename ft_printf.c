95be19b0ba2f278395b49e9353def745d886b462
Asmae Mouncif <asmounci@c3r3p1.1337.ma>
2025-10-27 10:34:21 +0100

    first version

---
+++ ft_printf.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asmounci <asmounci@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/27 09:01:16 by asmounci          #+#    #+#             */
+/*   Updated: 2025/10/27 10:29:39 by asmounci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_printf(const char *format, ...)
+{
+	int		i;
+	int		count;
+	va_list	args;
+
+	va_start(args, format);
+	i = 0;
+	count = 0;
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%' && format[i + 1] != '\0')
+		{
+			i++;
+			if (format[i] == 'c')
+				count = count + ft_putchar(va_arg(args, int));
+			else if (format[i] == 's')
+				count = count + ft_putstr(va_arg(args, char *));
+		}
+		else
+		{
+			count = count + ft_putchar(format[i]);
+		}
+		i++;
+	}
+	va_end(args);
+	return (count);
+}
+
+// int	main(void)
+// {
+// 	ft_printf("%s", "hello");
+// 	return (0);
+// }
---
+++ ft_printf.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asmounci <asmounci@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/27 09:23:40 by asmounci          #+#    #+#             */
+/*   Updated: 2025/10/27 10:28:46 by asmounci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+
+int	ft_printf(const char *format, ...);
+int	ft_putchar(int c);
+int	ft_putstr(char *str);
+
+#endif
